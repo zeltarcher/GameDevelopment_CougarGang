629448d27873aaf3885392856162fb1a8ab94892
rhwjwkdeks <rhwjwkdeks@gmail.com>
2020-10-07 14:24:05 -0500

    Drug script
    added to key picker

--- Escape_the_Lab/Assets/Actors/Player/KeyPicker.cs
+++ Escape_the_Lab/Assets/Actors/Player/KeyPicker.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class KeyPicker : MonoBehaviour
 {
     private float coin = 0;
+    private float key = 0;
+    private float drug = 0;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.transform.tag == "Keys")
         {
             Destroy(other.gameObject);
-        }    
+        }
+
+        else if(other.transform.tag == "Drugs")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
