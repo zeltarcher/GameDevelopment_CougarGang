a98e6c638dd02db01a76445c16e437a748f5554a
rbeer172 <70540400+rbeer172@users.noreply.github.com>
2020-10-08 19:48:47 -0500

    bug fixes and made prefabs more modular

---
+++ Escape_the_Lab/Assets/Objects/playerCamera.cs
@@ -0,0 +1,16 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour
+{
+    CinemachineVirtualCamera obj;
+    Player player;
+    void Start()
+    {
+        obj = GetComponent<CinemachineVirtualCamera>();
+        player = FindObjectOfType<Player>();
+        obj.Follow = player.GetComponent<Transform>();
+    }
+}
