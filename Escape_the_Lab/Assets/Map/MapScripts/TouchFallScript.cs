752a40c25a39bb9b092137843e31e3d5e61005d5
zrq2018 <62976219+zrq2018@users.noreply.github.com>
2020-10-31 23:16:03 -0500

    Oct31 Add Map Part2

---
+++ Escape_the_Lab/Assets/Map/MapScripts/TouchFallScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchFallScript : MonoBehaviour
+{
+    public float droppingDelay;
+    public float stopDelay;
+    public int touch;
+    public int maxTouch;
+    
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        touch++;
+        sr.sprite = Resources.Load("RedUnit", typeof(Sprite)) as Sprite;
+        if (touch > maxTouch) {
+
+            Invoke("StartDropping", droppingDelay);
+        }
+    }
+
+    void StartDropping()
+    {
+        rb.isKinematic = false;
+    }
+
+}
