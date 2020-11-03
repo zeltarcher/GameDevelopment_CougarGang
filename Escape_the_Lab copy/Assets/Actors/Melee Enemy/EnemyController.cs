4f1180582aae7c32632efb6fa94534f19c1dbd5e
rhwjwkdeks <rhwjwkdeks@gmail.com>
2020-11-03 14:55:00 -0600

    re

---
+++ Escape_the_Lab copy/Assets/Actors/Melee Enemy/EnemyController.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public LayerMask detectCollisionWith;
+    public float followRange = 5f;
+    public float attackRange = 1f;
+    public float gravitySpeed = 400f;
+    public float health = 100;
+    Transform player;
+    Rigidbody2D myRigidBody;
+    BoxCollider2D box;
+    PolygonCollider2D polygon;
+    Bounds colliderBounds;
+    SpriteRenderer sprite;
+    Vector2 leftRayCast, rightRayCast;
+    RaycastHit2D leftHit, rightHit;
+    Animator animate;
+    State state;
+    bool ground;
+    float distanceToPlayer;
+    float gravity;
+
+    //healthbar
+    Transform healthBar;
+    float hb_max;
+
+    private enum State 
+    { 
+        Walking, 
+        Chase, 
+        Attack,
+        Hit,
+        Death
+    }
+
+    private void playAnimation(String name)
+    {
+        foreach (AnimatorControllerParameter parameter in animate.parameters)
+        {
+            if (parameter.type == AnimatorControllerParameterType.Bool)
+            {
+                animate.SetBool(parameter.name, false);
+            }
+        }
+        animate.SetBool(name, true);
+    }
+
+    void updateState()
+    {
+        switch(state)
+        {
+            case State.Walking:
+                playAnimation("enemy walking");
+                if (leftHit != rightHit && ground == true)
+                {
+                    moveSpeed *= -1;
+                    ground = false;
+                }
+
+                if (Mathf.Sign(moveSpeed) > 0)
+                    sprite.flipX = false;
+                else
+                    sprite.flipX = true;
+           
+                myRigidBody.velocity = new Vector2(moveSpeed, gravity);
+                if (leftHit == rightHit)
+                    ground = true;
+
+                break;
+
+            case State.Chase:
+                playAnimation("enemy walking");
+                if (transform.position.x > player.position.x)
+                {
+                    sprite.flipX = true;
+                    moveSpeed = -Mathf.Abs(moveSpeed);
+                }
+                else
+                {
+                    moveSpeed = Mathf.Abs(moveSpeed);
+                    sprite.flipX = false;
+                }
+                myRigidBody.velocity = new Vector2(moveSpeed * 1.5f, gravity);
+                break;
+
+            case State.Attack:
+                playAnimation("enemy attack");
+                myRigidBody.velocity = new Vector2(0, gravity);
+                break;
+            case State.Hit:
+                myRigidBody.velocity = new Vector2(0, gravity);
+                break;
+            case State.Death:
+                playAnimation("enemy death");
+                health = 0;
+                foreach (Collider c in GetComponents<Collider>())
+                    c.enabled = false;
+                myRigidBody.velocity = new Vector2(0, gravity);
+                enabled = false;
+                break;
+        }
+    }
+
+    void updateRaycast()
+    {
+        colliderBounds = box.bounds;
+        leftRayCast = new Vector2(colliderBounds.min.x - .1f, colliderBounds.min.y);
+        rightRayCast = new Vector2(colliderBounds.max.x + .1f, colliderBounds.min.y);
+        leftHit = Physics2D.Raycast(leftRayCast, Vector2.down, .1f, detectCollisionWith);
+        rightHit = Physics2D.Raycast(rightRayCast, Vector2.down, .1f, detectCollisionWith);
+    }
+
+    void terminate() { Destroy(gameObject); }
+    void enableAttack() { polygon.enabled = true; }
+    void disableAttack() { polygon.enabled = false; }
+    void endHit() { state = State.Walking;}
+    public void TakeDamage(int damage) 
+    {
+        animate.SetTrigger("enemy hit");
+        state = State.Hit;
+        health -= damage;
+
+        if (health >= 0)
+        {
+            healthBar.localScale = new Vector3(health / hb_max, healthBar.localScale.y, healthBar.localScale.z);
+        }
+        
+    }
+    void poisonWater(){ TakeDamage(10); }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Platform" && leftHit && rightHit)
+            gravity = 0;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Platform")
+            gravity += -gravitySpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Water")
+        {
+            state = State.Hit;
+            InvokeRepeating("poisonWater", 0f, .5f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Water")
+        {
+            state = State.Walking;
+            CancelInvoke();
+        }
+    } 
+
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        player = FindObjectOfType<Player>().transform;
+        animate = GetComponent<Animator>();
+        polygon = GetComponentInChildren<PolygonCollider2D>();
+        polygon.enabled = false;
+        state = State.Walking;
+
+        healthBar = gameObject.transform.Find("HealthBar");
+        hb_max = health;
+    }
+
+
+    void FixedUpdate()
+    {
+        updateRaycast();
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (state != State.Hit && health > 0)
+        {
+            if (distanceToPlayer <= followRange && distanceToPlayer >= attackRange)
+                state = State.Chase;
+            else if (distanceToPlayer <= attackRange)
+                state = State.Attack;
+            else
+                state = State.Walking;
+        }
+        else if(health <= 0)
+        {
+            state = State.Death;
+        }
+
+        updateState();
+    }
+}
+
+
---
+++ Escape_the_Lab copy/Assets/Objects/rising water/water.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class water : MonoBehaviour
+{
+    public int testNumber = 0;
+<<<<<<< HEAD
+    public float waterSpeed = 100;
+    TilemapCollider2D ignoreCollider;
+=======
+    public float waterSpeed;
+>>>>>>> 4889e8bde82bcef306116f1ba28d527cc6727ecb
+    private Vector3 velocity;
+    SpriteRenderer sprite;
+    bool checkX = false;
+    bool checkY = false;
+<<<<<<< HEAD
+    bool hitWall = false;
+    Transform cameraTransform;
+
+
+=======
+>>>>>>> 4889e8bde82bcef306116f1ba28d527cc6727ecb
+    AudioSource water_AudioSrc;
+    AudioClip water_raisingSound;
+    //HealthController Health;
+    //public int damageAmount = 10;
+    //public float damageInterval = .5f;
+
+    private void flipSpriteX()
+    {
+        if (checkX == false)
+            sprite.flipX = true;
+        if (checkX == true)
+            sprite.flipX = false;
+
+        if (checkX == false)
+            checkX = true;
+        else
+            checkX = false;
+    }
+    private void flipSpriteY()
+    {
+        if (checkY == false)
+            sprite.flipY = true;
+        if (checkY == true)
+            sprite.flipY = false;
+
+        if (checkY == false)
+            checkY = true;
+        else
+            checkY = false;
+    }
+
+   /* private void poisonWater()
+    {
+        Health.TakeDamage(damageAmount);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.tag == "Enemy")
+        {
+            Debug.Log(collision.gameObject.name);
+            Health = collision.gameObject.GetComponent<HealthController>();
+            InvokeRepeating("poisonWater", 0f, damageInterval);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        CancelInvoke();
+    } */
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        waterSpeed = waterSpeed / 100000;
+        InvokeRepeating("flipSpriteX", 1f, .5f); 
+        InvokeRepeating("flipSpriteY", .8f, .5f);
+
+        water_raisingSound = Resources.Load<AudioClip>("Water_Rasing_Normal");
+        water_AudioSrc = GetComponent<AudioSource>();
+    }
+
+<<<<<<< HEAD
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        
+
+        // cameraBounds = OrthographicBounds(mainCamera);
+        // waterBounds = boxCollider.bounds;
+        //waterBounds.max.y < cameraBounds.max.y
+        //if (!hitWall)
+        //  {
+        
+        velocity.y += waterSpeed * Time.deltaTime;
+       
+        // }
+        // else
+         // velocity.y = 0;
+
+        //Vector3 resultingPosition = cameraTransform.position + cameraTransform.forward;
+        //transform.position = new Vector3(resultingPosition.x, transform.position.y, resultingPosition.z);
+=======
+    void FixedUpdate()
+    {
+            velocity.y += waterSpeed * Time.deltaTime;
+>>>>>>> 4889e8bde82bcef306116f1ba28d527cc6727ecb
+
+        transform.Translate(velocity);
+
+        //SFX
+        if (water_AudioSrc.time >= water_raisingSound.length || water_AudioSrc.time <= 0)
+        {
+            //Debug.Log("Get in play water sound==========================");
+            water_AudioSrc.Play();
+        }
+
+    }
+}
