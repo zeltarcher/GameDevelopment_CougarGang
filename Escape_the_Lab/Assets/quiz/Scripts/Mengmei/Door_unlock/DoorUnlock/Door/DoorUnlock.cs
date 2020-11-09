68d69d19785d0e9df432d5b3cf027cc7e8a8ddd2
zeltarcher <truongchiquang@gmail.com>
2020-11-09 03:35:33 -0600

    add Binh's quiz folder

---
+++ Escape_the_Lab/Assets/quiz/Scripts/Mengmei/Door_unlock/DoorUnlock/Door/DoorUnlock.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorUnlock : MonoBehaviour
+{
+    private bool collides;
+    private bool isOpened;
+    private Animator anim;
+    private LoadScene LoadNext;
+
+    //Password
+    public GameObject dialogBox;
+    private CheckPasscode checkPasscode;
+
+    void Start()
+    {
+        isOpened = false;
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+        LoadNext = GetComponent<LoadScene>();
+
+        dialogBox.SetActive(false);
+        checkPasscode = FindObjectOfType<CheckPasscode>();
+    }
+
+    void Update()
+    {
+        if (collides && Input.GetKeyDown(KeyCode.E))
+        {
+            dialogBox.SetActive(true);
+        }
+
+        if(collides && !isOpened && checkPasscode.CodePassed())
+        {
+            dialogBox.SetActive(false);
+            anim.enabled = true;
+            anim.SetTrigger("DoorUnlock");
+            isOpened = true;
+            LoadNext.LoadNextScene();
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.BoxCollider2D")
+        //{
+            collides = true;
+        //}
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.BoxCollider2D")
+        ///{
+            collides = false;
+            dialogBox.SetActive(false);
+        //}
+    }
+}
---
+++ Escape_the_Lab/Assets/quiz/Scripts/Mengmei/Door_unlock/DoorUnlock/Door/Quiz.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class Quiz : MonoBehaviour
+{
+    public struct QuizQuestion
+    {
+        public string question;
+        public string options;
+        public string answer;
+    }
+    private string xmlPath = "Assets/Quiz.xml";
+    private XmlDocument document;
+
+    private QuizQuestion Q;
+    public int count;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        document = ReadXml(xmlPath);
+        GetTotalNumOfQuestions();
+    }
+
+    private XmlDocument ReadXml(string path)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(path);
+
+        return doc;
+    }
+
+    public QuizQuestion FetchQuestion(int num)
+    {
+        //Fetch question from xml file
+        XmlNode Quiz = document.SelectSingleNode("quizBank");
+        XmlNodeList QuestionList = Quiz.ChildNodes;
+        XmlNode Question = QuestionList[num];
+        string que = Question.ChildNodes[0].InnerText;
+        string opt = "";
+        string[] C = { "A. ", "B. ", "C. ", "D. " };
+        for (int i = 1; i <= 4; i++)
+        {
+
+            opt += C[i-1] + Question.ChildNodes[i].InnerText+"\n";
+        }
+        string ans = Question.ChildNodes[1].InnerText;
+
+        //assign the all the info to a single class variable
+        Q.question = que;
+        Q.options = opt;
+        Q.answer = ans;
+
+        return Q;
+    }
+
+    public int GetTotalNumOfQuestions()
+    {
+        
+        XmlNode Quiz = document.SelectSingleNode("quizBank");
+        XmlNodeList QuestionList = Quiz.ChildNodes;
+        count = QuestionList.Count;
+        return count;
+    }
+}
