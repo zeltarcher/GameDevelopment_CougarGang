2e5f94e5dde0f4513085525ba7cd070141dfc38d
rbeer172 <70540400+rbeer172@users.noreply.github.com>
2020-11-16 19:05:46 -0600

    Added boss and bugfixes

--- Escape_the_Lab/Assets/Actors/Boss/Boss.cs
+++ Escape_the_Lab/Assets/Actors/Boss/Boss.cs
@@ -5,7 +5,7 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Timeline;
 
-public class EnemyController : MonoBehaviour
+public class Boss : MonoBehaviour
 {
     public float moveSpeed = 1f;
     public LayerMask detectCollisionWith;
@@ -19,8 +19,7 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D myRigidBody;
     BoxCollider2D box;
     PolygonCollider2D polygon;
-    CapsuleCollider2D capsule;
-    Bounds colliderBounds, capsuleBounds;
+    Bounds colliderBounds;
     SpriteRenderer sprite;
     Vector2 leftRayCast, rightRayCast, topRaycast;
     RaycastHit2D leftHit, rightHit, stunCheck, leftWall, rightWall;
@@ -38,10 +37,10 @@ public class EnemyController : MonoBehaviour
     AudioSource SFX_enemySrc;
     AudioClip enemy_dieSound, enemy_hitSound, enemy_slashing;
 
-    private enum State 
-    { 
-        Walking, 
-        Chase, 
+    private enum State
+    {
+        Walking,
+        Chase,
         Attack,
         Hit,
         Death,
@@ -71,32 +70,32 @@ public class EnemyController : MonoBehaviour
 
     void updateState()
     {
-        switch(state)
+        switch (state)
         {
             case State.Walking:
-                playAnimation("enemy walking");
+                playAnimation("boss walking");
                 if (leftHit != rightHit && ground == true)
                 {
                     moveSpeed *= -1;
                     ground = false;
                 }
 
-                if(leftWall || rightWall)
+                if (leftWall || rightWall)
                     moveSpeed *= -1;
 
                 if (Mathf.Sign(moveSpeed) > 0)
                     sprite.flipX = false;
                 else
                     sprite.flipX = true;
-           
+
                 myRigidBody.velocity = new Vector2(moveSpeed, gravity);
                 if (leftHit == rightHit)
                     ground = true;
-
+                Debug.Log("going through case");
                 break;
 
             case State.Chase:
-                playAnimation("enemy walking");
+                playAnimation("boss walking");
                 if (transform.position.x > player.position.x)
                 {
                     sprite.flipX = true;
@@ -111,24 +110,19 @@ public class EnemyController : MonoBehaviour
                 break;
 
             case State.Attack:
-                playAnimation("enemy attack");
+                playAnimation("boss idle melee attack");
                 myRigidBody.velocity = new Vector2(0, gravity);
-
-                if(!SFX_enemySrc.isPlaying)
-                    SFX_enemySrc.PlayOneShot(enemy_slashing);
 
                 break;
             case State.Hit:
                 myRigidBody.velocity = new Vector2(0, gravity);
                 break;
             case State.Death:
-                CoUpdate();
-
-                playAnimation("enemy death");
+                playAnimation("boss dying");
                 health = 0;
                 foreach (Collider c in GetComponents<Collider>())
                     c.enabled = false;
-                myRigidBody.velocity = new Vector2(0, gravity); 
+                myRigidBody.velocity = new Vector2(0, gravity);
                 enabled = false;
                 break;
             case State.Stunned:
@@ -140,23 +134,12 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    IEnumerable CoUpdate()
-    {
-        Debug.Log("Waiting");
-        if (!SFX_enemySrc.isPlaying)
-            SFX_enemySrc.PlayOneShot(enemy_dieSound);
-        yield return new WaitForSeconds(2);
-
-        yield return null;
-    }
-
     void updateRaycast()
     {
         colliderBounds = box.bounds;
-        capsuleBounds = capsule.bounds;
         leftRayCast = new Vector2(colliderBounds.min.x, colliderBounds.min.y);
-        rightRayCast = new Vector2(colliderBounds.max.x,colliderBounds.min.y);
-        topRaycast = new Vector2(capsuleBounds.center.x, capsuleBounds.max.y);
+        rightRayCast = new Vector2(colliderBounds.max.x, colliderBounds.min.y);
+        topRaycast = new Vector2(colliderBounds.center.x, colliderBounds.max.y);
         leftHit = Physics2D.Raycast(leftRayCast, Vector2.down, .1f, detectCollisionWith);
         rightHit = Physics2D.Raycast(rightRayCast, Vector2.down, .1f, detectCollisionWith);
         stunCheck = Physics2D.Raycast(topRaycast, Vector2.up, .05f, detectCollisionWith);
@@ -167,24 +150,16 @@ public class EnemyController : MonoBehaviour
     void terminate() { Destroy(gameObject); }
     void enableAttack() { polygon.enabled = true; }
     void disableAttack() { polygon.enabled = false; }
-    void endHit() { state = State.Walking;}
-    public void TakeDamage(int damage) 
+    void endHit() { state = State.Walking; }
+    public void TakeDamage(int damage)
     {
-        animate.SetTrigger("enemy hit");
+        animate.SetTrigger("boss hit");
         state = State.Hit;
         health -= damage;
 
-        if (health >= 0)
-        {
-            healthBar.localScale = new Vector3(health / hb_max, healthBar.localScale.y, healthBar.localScale.z);
-        }
-
-
-        if (!SFX_enemySrc.isPlaying)
-            SFX_enemySrc.PlayOneShot(enemy_hitSound);
 
     }
-    void poisonWater(){ TakeDamage(10); }
+    void poisonWater() { TakeDamage(10); }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -215,26 +190,18 @@ public class EnemyController : MonoBehaviour
             state = State.Walking;
             CancelInvoke();
         }
-    } 
+    }
 
     void Start()
     {
         box = GetComponent<BoxCollider2D>();
-        capsule = GetComponent<CapsuleCollider2D>();
         myRigidBody = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-        /*
-        if(FindObjectOfType<charChange>().p1 == true)
-            player = GameObject.Find("Man").GetComponent<Player>().transform;
-        else if(FindObjectOfType<charChange>().p2 == true)
-            player = GameObject.Find("Robot").GetComponent<Player>().transform;
-        */
         animate = GetComponent<Animator>();
         polygon = GetComponentInChildren<PolygonCollider2D>();
         polygon.enabled = false;
         state = State.Walking;
         gravity = -gravitySpeed / 100;
-
         healthBar = gameObject.transform.Find("HealthBar");
         hb_max = health;
 
@@ -264,17 +231,14 @@ public class EnemyController : MonoBehaviour
             else
                 state = State.Walking;
         }
-        else if(health <= 0)
+        else if (health <= 0)
         {
-            CoUpdate();
             state = State.Death;
         }
 
         if (stunCheck)
             state = State.Stunned;
-
+        Debug.Log("velocity is" + myRigidBody.velocity + "state is " + state);
         updateState();
     }
 }
-
-
