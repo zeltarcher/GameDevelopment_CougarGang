ac1b680a6cb4e3e3af7d96c432fe3f844dffa844
zeltarcher <truongchiquang@gmail.com>
2020-11-18 19:17:56 -0600

    logo scene
    This scene wiill be the first in player settings

---
+++ Escape_the_Lab/Assets/LogoLoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LogoLoadScene : MonoBehaviour
+{
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SceneManager.LoadScene(1);
+        StartCoroutine(LoadLevelAfterDelay(4));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Escape_the_Lab/Assets/Quang/TriggerChangeBGMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChangeBGMusic : MonoBehaviour
+{
+    private AudioSource aSource;
+    private AudioClip bgMusicLv2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GameObject.Find("BGMusic").GetComponent<AudioSource>(); 
+        bgMusicLv2 = Resources.Load<AudioClip>("Fire_Burning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
