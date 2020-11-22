bdd1311ff9579320ba49505a3428047cb668f79b
zrq2018 <62976219+zrq2018@users.noreply.github.com>
2020-11-21 20:18:01 -0600

    Done

---
+++ Escape_the_Lab/Assets/R/FixedGun/FireBulletSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBulletSpawner : MonoBehaviour
+{
+
+    public GameObject getBullet;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Start()
+    {
+        //InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    void Update()
+    {
+        //Instantiate(getBullet, transform.position, getBullet.transform.rotation);
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Instantiate(getBullet, transform.position, getBullet.transform.rotation);
+        }
+    }
+}
