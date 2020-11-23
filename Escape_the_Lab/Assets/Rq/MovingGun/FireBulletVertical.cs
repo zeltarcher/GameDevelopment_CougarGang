cb7e2cc041fcf517c5e31980809bed4fd06d8c00
zrq2018 <62976219+zrq2018@users.noreply.github.com>
2020-11-23 03:19:06 -0600

    Add Trap Gun and moving platform

---
+++ Escape_the_Lab/Assets/Rq/MovingGun/FireBulletVertical.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBulletVertical : MonoBehaviour
+{
+
+    public float fireSpeed = 20;
+    public float des;
+    public Vector3 targetPoint;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //float xv = this.transform.position.x;
+        targetPoint.x = this.transform.position.x;
+        targetPoint.y = this.transform.position.y - 2;
+        targetPoint.z = this.transform.position.z;
+
+        transform.position = Vector3.MoveTowards(this.transform.position, targetPoint, Time.deltaTime * fireSpeed);
+
+        des = des - fireSpeed;
+
+        if (des < 0.1f)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ Escape_the_Lab/Assets/Rq/MovingPlatform/PathMoving.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMoving : MonoBehaviour
+{
+    public GameObject[] gos = new GameObject[4];
+    //public GameObject targetOn;
+    public float speed = 1;
+    int i = 0;
+    float des;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        des = Vector3.Distance(this.transform.position, gos[i].transform.position);
+
+        transform.position = Vector3.MoveTowards(this.transform.position, gos[i].transform.position, Time.deltaTime * speed);
+
+        //vertical moving, need some adjustment on player
+
+        //if ((this.transform.position.x - gos[i].transform.position.x) > 0.3f)
+        //{
+
+        //}
+
+        //if ((gos[i].transform.position.x - this.transform.position.x) > 0.3f)
+        //{
+
+        //}
+
+        if (des < 0.3f && i < gos.Length)
+        {
+            i++;
+        }
+
+        if(i == gos.Length)
+        {
+
+            i = 0;
+
+        }
+
+    }
+
+}
