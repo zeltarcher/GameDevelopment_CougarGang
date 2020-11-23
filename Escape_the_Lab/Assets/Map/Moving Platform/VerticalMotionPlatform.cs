78a0791a7b820c85a05490e9bf24dfb711a5b61c
binh0206 <43211598+binh0206@users.noreply.github.com>
2020-11-22 22:08:48 -0600

    Add Binh's Scene
    Add Binh's Scene

--- Escape_the_Lab/Assets/Map/Moving Platform/VerticalMotionPlatform.cs
+++ Escape_the_Lab/Assets/Map/Moving Platform/VerticalMotionPlatform.cs
@@ -17,6 +17,11 @@ public class VerticalMotionPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log("y = " + transform.position.y);
+        Debug.Log(transform.position.y > Vertical_High_Point);
+        Debug.Log("Move up is: " + moveUp);
+        //Debug.Log("Vertical_High_Point = " + Vertical_High_Point);
+        //Debug.Log("Vertical_Low_Point = " + Vertical_Low_Point);
         if (transform.position.y > Vertical_High_Point)
         {
             moveUp = false;
@@ -28,12 +33,24 @@ public class VerticalMotionPlatform : MonoBehaviour
 
         if (moveUp)
         {
+            Debug.Log("Moving UPPPPPPPPPPP");
             transform.position = new Vector2(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime);
         }
         else
         {
+            Debug.Log("Moving Downnnnn");
             transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime);
         }
 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("AHHHHHHHHHHHHHHHHHHH");
+        if (collision.tag == "Platform")
+        {
+            //Debug.Log("Hit " + collision.tag);
+            // collision.transform.position = new Vector2(collision.transform.position.x + moveSpeed * Time.deltaTime *-1, collision.transform.position.y);
+            moveSpeed *= -1;
+        }
     }
 }
