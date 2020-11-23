0348274dd3880d42a74fee22b547e5fee2612141
Maggie He <hmm2381@gmail.com>
2020-11-23 13:00:28 -0600

    Shark added

---
+++ Escape_the_Lab/Assets/Objects/Sharks/SharkSpawner.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class SharkSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject shark;
+    [SerializeField]
+    private Waves[] wave;
+    private float currentTime;
+    private int waveIndex;
+    private Player player;
+    private Vector3 velocity;          //used to check whether the player starts moving
+    private bool StartSpawn = false;
+    private float xPos = 0.0f;
+    GameObject risingWater;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveIndex = 0;
+        currentTime = wave[waveIndex].delayTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        risingWater = GameObject.FindGameObjectWithTag("Water");
+
+        if (FindObjectOfType<charChange>().p1 == true)
+        {
+            player = GameObject.Find("Man").GetComponent<Player>();
+            velocity = player.GetPlayerVelocity();
+        }
+        else if (FindObjectOfType<charChange>().p2 == true)
+        {
+            player = GameObject.Find("Robot").GetComponent<Player>();
+            velocity = player.GetPlayerVelocity();
+        }
+
+        if (velocity.x > 0)     
+        {
+            StartSpawn = true;
+        }
+
+        if (StartSpawn == true)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime < 0)
+                SelectWave();
+        }
+    }
+
+    void SpawnSharks(float x)
+    { 
+        GameObject obj = Instantiate(shark);
+        float y = risingWater.transform.position.y + 2 * risingWater.transform.localScale.y;
+        obj.transform.position = new Vector3(x, y, 0);
+    }
+
+    void SelectWave()
+    {
+        currentTime = wave[waveIndex].delayTime;
+        if (wave[waveIndex].totalSpawn > 0)
+        {
+            xPos = Random.Range(player.transform.position.x - 20, player.transform.position.x + 20);
+            SpawnSharks(xPos);
+            wave[waveIndex].totalSpawn--;    
+        }
+        else
+        {
+            if (waveIndex < wave.Length - 1)
+                waveIndex++;
+            else
+                wave[waveIndex].totalSpawn = 10;
+        }
+
+    }
+}
+
+[System.Serializable]
+public class Waves
+{
+    public float delayTime;
+    public float totalSpawn;
+}
