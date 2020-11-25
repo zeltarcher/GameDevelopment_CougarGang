ddf6255d94f813af70e3e01054364ec11fef95f2
zrq2018 <62976219+zrq2018@users.noreply.github.com>
2020-11-25 09:57:26 -0600

    shark

---
+++ Escape_the_Lab/Assets/R/shark/Shark.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shark : MonoBehaviour
+{
+    public GameObject[] gos1 = new GameObject[2];
+    //public GameObject targetOn;
+    public float speed1 = 1;
+    int i = 0;
+    float des;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        des = Vector3.Distance(this.transform.position, gos1[i].transform.position);
+
+        transform.position = Vector3.MoveTowards(this.transform.position, gos1[i].transform.position, Time.deltaTime * speed1);
+
+        if (des < 0.3f && i < gos1.Length)
+        {
+            i++;
+        }
+
+        if (i == gos1.Length)
+        {
+
+            i = 0;
+            this.transform.position = gos1[i].transform.position;
+
+        }
+
+    }
+}
